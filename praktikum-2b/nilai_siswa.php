909ca8b050dbfaa790bce2ba0ab9c4df25f00862
Ahmad Huda Salam <ahmadhudasalam9@gmail.com>
2025-03-02 07:11:54 +0700

    praktikum 2

---
+++ praktikum-2b/nilai_siswa.php
@@ -0,0 +1,50 @@
+<?php
+/*
+- MENANGKAP DATA YANG DI-INPUT
+*/
+
+
+
+/*
+- Mendefinisikan Variabel
+- Nilai Akhir
+- Grade
+- Predikat
+*/
+
+
+
+/*
+- MENENTUKAN LULUS ATAU TIDAK MENGGUNAKAN IF ELSE
+- SISWA DINYATAKAN LULUS JIKA NILAI TOTAL dengan presentase 30% UTS, 35% UAS dan TUGAS 35% melebihi 55
+*/
+
+
+
+// MENENTUKAN GRADE NILAI MENGGUNAKAN SYNTAX IF ELSE MULTIKONDISI
+/*
+- Grade E : Jika Nilai Akhir 0-35
+- Grade D : Jika Nilai Akhir 36-55
+- Grade C : Jika Nilai Akhir 56-69
+- Grade B : Jika Nilai Akhir 70-84
+- Grade A : Jika Nilai Akhir 85-100
+- Grade I : Jika Nilai Akhir < 0 atau Nilai Akhir > 100
+*/
+
+
+
+// MENENTUKAN PREDIKAT NILAI MENGGUNAKAN SYNTAX SWITCH
+/*
+- Predikat Sangat Kurang : Jika Grade E
+- Predikat Kurang : Jika Grade D
+- Predikat Cukup : Jika Grade C
+- Predikat Memuaskan : Jika Grade B
+- Predikat Sangat Memuaskan : Jika Grade A
+- Predikat Tidak Ada : Jika Grade I
+*/
+
+
+// MENCETAK HASIL
+// if (!empty($proses)) {
+//     echo 'Proses : ' . $proses;
+// }
